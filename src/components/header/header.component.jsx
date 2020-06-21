198033aa99d36dd0d4b4f14d6fe12a43d38067b5
Bon Huynh <bon.huynh@dcyf.wa.gov>
2020-06-21 08:46:35 -0700

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -38,7 +38,7 @@ const Header = ({ currentUser, hidden }) => (
       <CartIcon />
     </div>
     {
-      hidden ? null: <CartDropdown />
+      hidden ? null : <CartDropdown />
     }
   </div>
 );
